137fdd53cc3d7e80922eff6dd4cbbc5525607f2f
ioanabadea6 <119163461+ioanabadea6@users.noreply.github.com>
2023-05-28 00:53:23 +0300

    Add files via upload

--- a3/a3.c
+++ a3/a3.c
@@ -43,6 +43,12 @@ int main(){
             int shmFD=0;
             volatile char *data = NULL;
             unsigned int size_shm = 4899052; 
+            unsigned int offset = 0;
+            unsigned int value = 0;
+            char file_name[250];
+            int fd_file_name = -1;
+            volatile char *data2 = NULL;
+            int size2;
 
             read(fd2, &c, sizeof(c));
             while(c != '!'){
@@ -59,14 +65,13 @@ int main(){
                 write(fd1, &nr, sizeof(nr));
             }
             else if(strcmp((char*)buffer,"CREATE_SHM") == 0){
-               // read(fd2, &size_shm, sizeof(unsigned int));
                 shmFD = shm_open("/iljVHa", O_CREAT | O_RDWR, 0664);
                 if(shmFD == -1){
                     write(fd1, "CREATE_SHM!", strlen("CREATE_SHM!"));
                     write(fd1, "ERROR!", strlen("ERROR!"));
                 }
                 ftruncate(shmFD, size_shm);
-                data = (char*)mmap(NULL, size_shm, PROT_READ | PROT_WRITE, MAP_SHARED, shmFD, 0);
+                data = (volatile char*)mmap(NULL, size_shm, PROT_READ | PROT_WRITE, MAP_SHARED, shmFD, 0);
                 if(data == (void*)-1){
                     write(fd1, "CREATE_SHM!", strlen("CREATE_SHM!"));
                     write(fd1, "ERROR!", strlen("ERROR!"));
@@ -75,14 +80,45 @@ int main(){
                 write(fd1, "SUCCESS!", strlen("SUCCESS!"));
 
             }
+            else if(strcmp((char*)buffer, "WRITE_TO_SHM") == 0){   
+                read(fd2, &offset, sizeof(offset));    
+                read(fd2, &value, sizeof(value));
+                if((offset < 0 || offset > size_shm) || offset+sizeof(value)>size_shm){
+                    write(fd1, "WRITE_TO_SHM!", strlen("WRITE_TO_SHM!"));
+                    write(fd1, "ERROR!", strlen("ERROR!"));
+                } else{
+                memcpy((char*)&data[offset], &value, sizeof(value));
+                write(fd1, "WRITE_TO_SHM!", strlen("WRITE_TO_SHM!"));
+                write(fd1, "SUCCESS!", strlen("SUCCESS!"));}
+                
+            }
+            else if(strcmp((char*)buffer, "MAP_FILE")==0){
+                read(fd2, &file_name, strlen(file_name));
+                file_name[strlen(file_name)]='\0';
+                fd_file_name = open(file_name, O_RDONLY);
+                if(fd_file_name == -1){
+                    write(fd1, "MAP_FILE!", strlen("MAP_FILE!"));
+                    write(fd1, "ERROR!", strlen("ERROR!"));
+                }
+                size2=lseek(fd_file_name, 0, SEEK_END);
+                data2 = (char*)mmap(NULL, size2, PROT_READ, MAP_SHARED, fd_file_name, 0);
+                if(data2 == (void*)-1){
+                    write(fd1, "MAP_FILE!", strlen("MAP_FILE!"));
+                    write(fd1, "ERROR!", strlen("ERROR!"));
+                }
+                write(fd1, "MAP_FILE!", strlen("MAP_FILE!"));
+                write(fd1, "SUCCESS!", strlen("SUCCESS!"));
+            }
             else if(strcmp((char*)buffer, "EXIT") == 0){
+                close(fd_file_name);
                 munmap((void*)data, size_shm);
+                munmap((void*)data2, size2);
                 data = NULL;
+                data2 = NULL;
                 close(fd2);
                 close(fd1);
                 unlink(PIPE1);
                 shm_unlink("/iljVHa");
-                //break;
             }
             else{
                 break;
